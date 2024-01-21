d902911af11ef2d7c696635f40d871fd921ebc16
Tobie-Rathbun <tsrathbun@gmail.com>
2024-01-21 07:19:55 -0700

    Internet Example fetch Public IP

--- Internet_Examples/Send_Video_in_Chunks_TCP-IP_Socket_Python.py
+++ Internet_Examples/Send_Video_in_Chunks_TCP-IP_Socket_Python.py
@@ -1,14 +1,42 @@
 import os
 import pickle
 import socket
+import requests
+
+
+
 
 CHUNK_SIZE = 1024  # chunk the data into 1024-byte pieces
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+
+
+
+def get_public_ip():
+    try:
+        # Use a service that returns your public IP address
+        response = requests.get('https://api64.ipify.org?format=json')
+        
+        if response.status_code == 200:
+            # Parse the JSON response to extract the IP address
+            public_ip = response.json()['ip']
+            return public_ip
+        else:
+            print(f"Failed to get public IP. Status code: {response.status_code}")
+    except Exception as e:
+        print(f"Error getting public IP: {e}")
+
 # Get Public IP Address
-PUBLIC_IP = '10.18.18.162'
+PUBLIC_IP = get_public_ip()
+
+if PUBLIC_IP:
+    print(f'Public IP Address: {PUBLIC_IP}')
+else:
+    print('Unable to retrieve public IP address.')
+
+
 
 # Connect the socket to the server
 server_address = (PUBLIC_IP, 10000)
